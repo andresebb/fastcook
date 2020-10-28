3f659b4784c91845268cbdc545fb08fe3626db42
Andres Betancourt <aebb001@gmail.com>
2020-10-28 10:10:50 -0400

    Initializing project

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <h2>Fast Food</h2>
+      <p>Discover and get great food recipes</p>
+    </header>
+  );
+};
---
+++ src/components/listFoodCategories/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ListFoodCategories = () => {
+  return (
+    <div>
+      <h1>SOy la food categories</h1>
+    </div>
+  );
+};
